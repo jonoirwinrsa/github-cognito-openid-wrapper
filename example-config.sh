3d9c70ce3df575666b60281ed1fbff073e51c7b7
Jonathan Irwin <jono@sov.tech>
2019-11-06 15:23:27 +0200

    docs: remove ref to slack enterprise

--- example-config.sh
+++ example-config.sh
@@ -5,10 +5,6 @@ export SLACK_CLIENT_ID=# <Slack OAuth App Client ID>
 export SLACK_CLIENT_SECRET=# <Slack OAuth App Client Secret>
 export COGNITO_REDIRECT_URI=# https://<Your Cognito Domain>/oauth2/idpresponse
 
-# Variables required if used with Slack Enterprise
-# SLACK_API_URL=# https://<Slack Enterprise Host>/api/v3
-# SLACK_LOGIN_URL=# https://<Slack Enterprise Host>
-
 # Variables required if Splunk logger is used
 # SPLUNK_URL=# https://<Splunk HEC>/services/collector/event/1.0
 # SPLUNK_TOKEN=# Splunk HTTP Event Collector token
@@ -20,6 +16,3 @@ export COGNITO_REDIRECT_URI=# https://<Your Cognito Domain>/oauth2/idpresponse
 export BUCKET_NAME=# An S3 bucket name to use as the deployment pipeline
 export STACK_NAME=# The name of the stack to create
 export REGION=# AWS region to deploy the stack and bucket in
-
-# Variables required if deploying a node http server
-export PORT=# <Port to start the server on>
